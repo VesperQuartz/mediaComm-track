144a6eb249153007a5fff1188eb2cd495b87aa3f
VesperQuartz <lilbrown3000@gmail.com>
2026-03-02 10:40:45 +0100

    init commit

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,9 +7,9 @@ export async function proxy(request: NextRequest) {
   // THIS IS NOT SECURE!
   // This is the recommended approach to optimistically redirect users
   // We recommend handling auth checks in each page/route
-  if (!sessionCookie) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
+  // if (!sessionCookie) {
+  //   return NextResponse.redirect(new URL("/sign-in", request.url));
+  // }
 
   return NextResponse.next();
 }
